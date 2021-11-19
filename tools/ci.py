9df049b6ca35b995d5d774dfb46eaff091f604a1
noaione <noaione0809@gmail.com>
2021-11-19 23:32:13 +0800

    ci: update with new script and more

--- tools/ci.py
+++ tools/ci.py
@@ -12,12 +12,12 @@ base_dirs = [
 
 base_dir: Path = None
 for base in base_dirs:
-    if os.path.isfile(base / "main.r"):
+    if os.path.isfile(base / "main.R"):
         base_dir = base
         break
 
-main_r = base_dir / "main.r"
-main_ci_r = base_dir / "main_ci.r"
+main_r = base_dir / "main.R"
+main_ci_r = base_dir / "main_ci.R"
 
 print(f"📜 Working directory: {base_dir}")
 
@@ -25,13 +25,25 @@ print("📍 Reading main.r")
 with open(main_r, "r") as f:
     lines = f.readlines()
 
+
+def should_comment(line: str) -> bool:
+    if line.startswith("install.package"):
+        return True
+    if line.startswith("windows()"):
+        return True
+    if line.startswith("View()"):
+        return True
+    return False
+
+
 print("💉 Injecting function...")
 new_format: List[str] = []
 for line in lines:
-    if line.strip() == "":
-        new_format.append(line)
-    else:
-        new_format.append((" " * 4) + line)
+    if should_comment(line):
+        line = "# " + line
+    if line.strip() != "":
+        line = (" " * 4) + line
+    new_format.append(line)
 new_format.insert(0, "main <- function() {\n")
 new_format.append("}\n")
 new_format.append("\nmain()\n")
